993e8153ec60e069872515c24f13a807b54953fe
jlindell <jakob.lindell@gmail.com>
2023-10-15 19:18:53 +0200

    Initial Commit

---
+++ uppgift3/uppgift3App.swift
@@ -0,0 +1,17 @@
+//
+//  uppgift3App.swift
+//  uppgift3
+//
+//  Created by Jakob Lindell on 2023-10-15.
+//
+
+import SwiftUI
+
+@main
+struct uppgift3App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
